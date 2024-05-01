a9e0e7f0c3b23ff9c7cc1b1767facdf5ba0c929f
Mriacleliu <1973629453@qq.com>
2024-05-01 23:34:59 +0800

    调整route格式

--- app/route.php
+++ app/route.php
@@ -22,44 +22,45 @@ use think\Route;
  * @return void
  */
 //Route::rule('hello/:id', 'sample/Test/hello', 'GET|POST');
-// Test
+// Test API
 Route::get('api/:version/second', 'api/:version.Address/second');
 Route::get('api/:version/three', 'api/:version.Address/three');
 
+// ProductImg API
 Route::get('api/:version/banner/:id', 'api/:version.Banner/getBanner');
-
-Route::get('api/:version/theme','api/:version.Theme/getSimpleList');
-Route::get('api/:version/theme/:id','api/:version.Theme/getComplexOne');
+Route::get('api/:version/theme', 'api/:version.Theme/getSimpleList');
+Route::get('api/:version/theme/:id', 'api/:version.Theme/getComplexOne');
+Route::get('api/:version/category/all', 'api/:version.Category/getAllCategories');
 
 /*Route::get('api/:version/product/by_category/:id','api/:version.Product/getAllInCategory');
 Route::get('api/:version/product/:id','api/:version.Product/getOneProduct', [], ['id' => '\d+']);
 Route::get('api/:version/product/recent','api/:version.Product/getRecent');*/
-//Product API
-Route::group('api/:version/product', function (){
+
+// Product API
+Route::group('api/:version/product', function () {
     Route::get('/by_category/:id', 'api/:version.Product/getAllInCategory');
-    Route::get('/:id','api/:version.Product/getOneProduct', [], ['id' => '\d+']);
-    Route::get('/recent','api/:version.Product/getRecent');
+    Route::get('/:id', 'api/:version.Product/getOneProduct', [], ['id' => '\d+']);
+    Route::get('/recent', 'api/:version.Product/getRecent');
 });
 
-Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
 
-//Token API
+// Token API
 Route::post('api/:version/token/user', 'api/:version.Token/getToken');
 Route::post('api/:version/token/verify', 'api/:version.Token/verifyToken');
-Route::post('api/:version/token/app','api/:version.Token/getAppToken');
+Route::post('api/:version/token/app', 'api/:version.Token/getAppToken');
 
-//Address API
+// Address API
 Route::get('api/:version/address', 'api/:version.Address/getUserAddress');
 Route::post('api/:version/address', 'api/:version.Address/createOrUpdateAddress');
 
-//OrderAPI
+// OrderAPI
 Route::get('api/:version/order/by_user', 'api/:version.Order/getSummaryByUser');
-Route::get('api/:version/order/paginate','api/:version.Order/getSummary');
-Route::get('api/:version/order/:id', 'api/:version.Order/getDetail',[], ['id' => '\d+']);
+Route::get('api/:version/order/paginate', 'api/:version.Order/getSummary');
+Route::get('api/:version/order/:id', 'api/:version.Order/getDetail', [], ['id' => '\d+']);
 Route::post('api/:version/order', 'api/:version.Order/placeOrder');
 Route::put('api/:version/order/delivery', 'api/:version.Order/delivery');
 
-//Pay API
+// Pay API
 Route::post('api/:version/pay/pre_order', 'api/:version.Pay/getPreOrder');
 Route::post('api/:version/pay/notify', 'api/:version.Pay/receiveNotify');
 Route::post('api/:version/pay/re_notify', 'api/:version.Pay/redirectNotify');
